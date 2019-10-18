be57286a8d98160574f4acb2738b862b2c25ebb9
takoyaki28 <takoyaki.koume@gmail.com>
2019-10-18 22:14:09 +0900

    ファビコン

--- contents/js/main.js
+++ contents/js/main.js
@@ -20,23 +20,6 @@ jQuery(document).ready(function( $ ) {
 });
 
 $(function(){
-	//テキストの読み込み
-	//$("#txtLoad").load("DATA/sample.txt")
-
-	$("#load").toggle(
-		function(){
-			//リンクをクリックするとsample.txtが読み込まれ、リセット用のリンクテキストが表示されます
-			$(this).text("リセット")
-			$("#txtLoad").load("DATA/sample.txt")
-			},
-		function(){
-			//リセット用のテキストがクリックされた後の処理になります
-			$(this).text("読み込む")
-			$("#txtLoad").text("読み込んでいません")
-	});
-});
-
-$(function(){
   $("#header").load("../../header.html");
   $("#footer").load("../../footer.html");
 });
